6c9f88ad4bac49657525cc5ee9818666533852c8
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-08-06 19:20:22 +0530

    fix: resolve 4 bugs in take-one-nexus

--- routes/posts.js
+++ routes/posts.js
@@ -21,7 +21,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 const upload = multer({
-  storage: storage,
+  storage,
   fileFilter: fileFilter,
   limits: { fileSize: 10 * 1024 * 1024 } // 10MB Total Limit
 });
